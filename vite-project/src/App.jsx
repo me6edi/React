54677ca0073c01cbe733dbc5d437b00cd015c528
me6edi <exited17@gmail.com>
2024-10-09 20:10:16 +0600

    setAttribute

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,20 @@
+import { useRef } from "react";
+
 const App = () => {
+  let myImg = useRef();
 
-    const PostFormData=(event)=>{
-        event.preventDefault();
+  const change = () => {
+    myImg.current.src = "https://placehold.co/600x400?text=Hello+World";
+    myImg.current.setAttribute("height", "200px");
+    myImg.current.setAttribute("width", "300px");
+  };
+  return (
+    <div>
+      <img ref={myImg} src="https://placehold.co/500x400" alt="" />
 
-        // TOD
-        alert('Form submitted!')
-    }
-
-return(
-<div>
-    <form onSubmit={PostFormData}>
-        <input placeholder="name" />
-        <button type="submit">Submit</button>
-    </form>
-</div>
-
-);
-
-
+      <button onClick={change}>Click</button>
+    </div>
+  );
 };
 
 export default App;
