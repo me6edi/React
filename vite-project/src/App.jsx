6d4d0d03e454300ac6dc6a072130b3a51b4bf268
me6edi <exited17@gmail.com>
2024-10-22 21:09:18 +0600

    Ract hooks multiple object using useState

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 
 const App = () => {
-  const [number, setNumber] = useState(0);
+  const [myObj, setMyobj] = useState({
+    key1: "Value 1",
+    key2: "Value 2",
+    key3: "Value 3",
+  });
+
+  const change = () => {
+    setMyobj((prevObj) => ({
+      ...prevObj,
+      key1: "New value for key 1",
+      key2: "New value for key 2",
+      key3: "New value for key 3",
+    }));
+  };
 
   return (
     <div>
-      <h1>Number: {number}</h1>
-      <button onClick={() => setNumber(number + 1)}>Click</button>
+      <h1>{myObj.key1}</h1>
+      <h1>{myObj.key2}</h1>
+      <h1>{myObj.key3}</h1>
+      <button onClick={change}>Change</button>
     </div>
   );
 };
