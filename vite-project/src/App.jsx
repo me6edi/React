640a434587035cbd7093cd43fe2c172cc63484c8
me6edi <exited17@gmail.com>
2024-10-30 21:41:58 +0600

    Create Form With useState Hook In ReactJS

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,50 +1,77 @@
 import { useState } from "react";
 
 const App = () => {
-  const [list, setList] = useState([]);
-  const [item, setItem] = useState("");
+  let [FormObj, SetFormObj] = useState({
+    fname: "",
+    lname: "",
+    city: "",
+    gender: "",
+  });
 
-  const AddToList = () => {
-    list.push(item);
-    setList([...list]);
+  const InputOnChange = (property, value) => {
+    SetFormObj((prevObj) => ({
+      ...prevObj,
+      [property]: value,
+    }));
   };
 
-  const RemoveItem = (index) => {
-    list.splice(index, 1);
-    setList([...list]);
+  const FormSubmit = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(FormObj));
   };
 
   return (
-    <div>
-      <table>
-        <tbody>
-          {list.length !== 0 ? (
-            list.map((element, index) => {
-              return (
-                <tr>
-                  <td>{element}</td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        RemoveItem(index);
-                      }}
-                    >
-                      Remove
-                    </button>
-                  </td>
-                </tr>
-              );
-            })
-          ) : (
-            <tr></tr>
-          )}
-        </tbody>
-      </table>
-
-      <input onChange={(e) => setItem(e.target.value)} placeholder="item" />
-      <button onClick={AddToList}>Add</button>
+    <div className="container">
+      <form onSubmit={FormSubmit}>
+        <input
+          onChange={(e) => {
+            InputOnChange("fname", e.target.value);
+          }}
+          value={FormObj.fname}
+          placeholder="First Name"
+        />
+        <br />
+        <input
+          onChange={(e) => {
+            InputOnChange("lname", e.target.value);
+          }}
+          value={FormObj.lname}
+          placeholder="Last Name"
+        />
+        <br />
+        <select
+          onChange={(e) => {
+            InputOnChange("city", e.target.value);
+          }}
+          value={FormObj.city}
+        >
+          <option value="">Choose City</option>
+          <option value="Dhaka">Dhaka</option>
+          <option value="Rangpur">Rangpur</option>
+        </select>
+        <br />
+        <input
+          onChange={() => {
+            InputOnChange("gender", "Male");
+          }}
+          checked={FormObj.gender === "Male"}
+          type="radio"
+          name="gender"
+        />
+        Male
+        <input
+          onChange={() => {
+            InputOnChange("gender", "Female");
+          }}
+          checked={FormObj.gender === "Female"}
+          type="radio"
+          name="gender"
+        />
+        Female
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
-
 export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "milligram/dist/milligram.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
