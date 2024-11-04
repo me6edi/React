6d85d7364974bdbb4512c46de663a6da9c7ea009
me6edi <exited17@gmail.com>
2024-11-04 19:39:59 +0600

    Async and Await in react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
-import JSON from "./../node_modules/vite/dist/node/chunks/dep-BWSbWtLw";
 
 const App = () => {
   let [data, setData] = useState();
   useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((res = res.JSON()))
-      .then((json) => setData(json));
+    (async () => {
+      let response = await fetch("https://dummyjson.com/products/");
+      let json = await response.json();
+      setData(json);
+    })();
   }, []);
+
   return <div>{JSON.stringify(data)}</div>;
 };
 
