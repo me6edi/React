44efc425b9d55257863dba191de0c5e6ef2d68d2
me6edi <exited17@gmail.com>
2024-11-04 18:43:39 +0600

    useEffect Promises

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,77 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import JSON from "./../node_modules/vite/dist/node/chunks/dep-BWSbWtLw";
 
 const App = () => {
-  let [FormObj, SetFormObj] = useState({
-    fname: "",
-    lname: "",
-    city: "",
-    gender: "",
-  });
-
-  const InputOnChange = (property, value) => {
-    SetFormObj((prevObj) => ({
-      ...prevObj,
-      [property]: value,
-    }));
-  };
-
-  const FormSubmit = (e) => {
-    e.preventDefault();
-    alert(JSON.stringify(FormObj));
-  };
-
-  return (
-    <div className="container">
-      <form onSubmit={FormSubmit}>
-        <input
-          onChange={(e) => {
-            InputOnChange("fname", e.target.value);
-          }}
-          value={FormObj.fname}
-          placeholder="First Name"
-        />
-        <br />
-        <input
-          onChange={(e) => {
-            InputOnChange("lname", e.target.value);
-          }}
-          value={FormObj.lname}
-          placeholder="Last Name"
-        />
-        <br />
-        <select
-          onChange={(e) => {
-            InputOnChange("city", e.target.value);
-          }}
-          value={FormObj.city}
-        >
-          <option value="">Choose City</option>
-          <option value="Dhaka">Dhaka</option>
-          <option value="Rangpur">Rangpur</option>
-        </select>
-        <br />
-        <input
-          onChange={() => {
-            InputOnChange("gender", "Male");
-          }}
-          checked={FormObj.gender === "Male"}
-          type="radio"
-          name="gender"
-        />
-        Male
-        <input
-          onChange={() => {
-            InputOnChange("gender", "Female");
-          }}
-          checked={FormObj.gender === "Female"}
-          type="radio"
-          name="gender"
-        />
-        Female
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+  let [data, setData] = useState();
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res = res.JSON()))
+      .then((json) => setData(json));
+  }, []);
+  return <div>{JSON.stringify(data)}</div>;
 };
+
 export default App;
