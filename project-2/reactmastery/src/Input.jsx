11e129d86917622600cb1f0fb23289215c7f333b
me6edi <exited17@gmail.com>
2025-01-07 22:01:04 +0600

    React Form

--- project-2/reactmastery/src/Input.jsx
+++ project-2/reactmastery/src/Input.jsx
@@ -1,29 +1,37 @@
 import React, { Component } from 'react'
-
 export default class Input extends Component {
+    state = {
+      ChannelName:"",
+      category:"",
+      creationDate:"",
+      About:"",
+      gender:"",
+      checkbox:false,
+    }
     FormChange = (event)=>{
-        console.log(event.target.value)
+        this.setState({
+          [event.target.name]: event.target.value
+        })
     }
     CheckboxHandler=(event)=>{
       this.setState({
-        checkBox:event.target.checked
+        checkbox:event.target.checked
       })
     }
   render() {
+    const {ChannelName,Subscriber,category,creationDate,checkbox} = this.state
     return (
       <div className='container'>
-        <input type="text"  name='ChannelName' className='form-control' placeholder='Channel Name' onChange={this.FormChange}/>
-        <input type="text"  name='ChannelName' className='form-control' placeholder='Subscriber' onChange={this.FormChange}/>
-
-        <select name="category" onChange={this.FormChange} className='form-control'>
+        <input type="text"  name='ChannelName' className='form-control' placeholder='Channel Name' onChange={this.FormChange} value={ChannelName}/>
+        <input type="text"  name='Subscriber' className='form-control' placeholder='Subscriber' onChange={this.FormChange} value={Subscriber}/>
+        <select name="category" onChange={this.FormChange} className='form-control' value={category}>
           <option value="programming">Select</option>
           <option value="programming">Programming</option>
           <option value="funny">Funny</option>
           <option value="educations">Educations</option>
           <option value="entertainment">Entertainment</option>
         </select>
-
-        <input type="date" name='CreationDate' onChange={this.FormChange} className='form-control'/>
+        <input type="date" name='creationDate' onChange={this.FormChange} className='form-control' value={creationDate}/>
         <textarea name="About" onChange={this.FormChange} placeholder='Tell Me About YourSelf' className='form-control'></textarea>
         <div>
           <input type="radio" value= "Male" name="gender" onChange={this.FormChange}/>
@@ -31,7 +39,12 @@ export default class Input extends Component {
           <input type="radio" value="Female" name="gender" onChange={this.FormChange}/>
           <label>Female</label>
         </div>
-        <input type="checkbox" name="checkbox" onChange={this.CheckboxHandler}/>
+        <input type="checkbox" name="checkbox" checked={checkbox} onChange={this.CheckboxHandler}/>
+        <label>I Agree & Continue</label>
+        <br />
+        <button className='btn btn-primary' onClick={()=> console.log(this.state)}>
+          Submit
+        </button>
       </div>
     )
   }
