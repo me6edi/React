21079adc22145fab2a32ad08b7e87fb2d293ccad
me6edi <exited17@gmail.com>
2025-01-28 23:25:48 +0600

    navbar

--- project-3/blogwebsite/src/App.js
+++ project-3/blogwebsite/src/App.js
@@ -1,10 +1,13 @@
 // src/App.js
 import React from 'react';
 import Carousel from './Pages/BlogWebsite/Carousel';
+import Navbar from './Pages/BlogWebsite/Navbar'
+
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Carousel/>
     </div>
   );
--- project-3/blogwebsite/src/Pages/BlogWebsite/Carousel.jsx
+++ project-3/blogwebsite/src/Pages/BlogWebsite/Carousel.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-
+import "./BlogCSS/Blog.css"
 function Carousel() {
   return (
 <div>
-<div id="carouselExampleFade" class="carousel slide carousel-fade container" data-bs-ride="carousel">
+<div id="carouselExampleFade" class="carousel slide carousel-fade container mt-5" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/img1.jpg" class="d-block w-100" alt="..."/>
+      <div className="row">
+        <div className="col-md-6">
+        <img src="img/img1.jpg" class="d-block w-100" alt="..."/>
+        </div>
+        <div className="col-md-6">
+          <p className='ShortTitle'>Lorem ipsum dolor sit amet.</p>
+          <h3 className='LongTitle'>Live With Mehedi Amin</h3>
+          <p className='ShortTitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores, nostrum totam, error veritatis nesciunt, officiis accusantium corporis rerum non repellendus aliquid fugiat quaerat cupiditate fugit corrupti dolore! Voluptatem a delectus vel totam non voluptas cum. Voluptates est itaque assumenda nihil delectus nulla sunt, voluptas nostrum. Corporis at minus est natus.</p>
+          <p className='ChannelName'>Mehedi Amin</p>
+          <p className='ShortTitle'>27 Jan 2025</p>
+        </div>
+      </div>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
