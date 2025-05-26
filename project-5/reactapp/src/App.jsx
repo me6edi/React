a0b6a3dc430ab3f3a43b7bd2382bd35f7b0091ac
me6edi <exited17@gmail.com>
2025-05-26 12:58:44 +0600

    Generating fake data also using Tailwind css

--- project-5/reactapp/src/App.jsx
+++ project-5/reactapp/src/App.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import Singleuserdata from './components/Singleuserdata/Singleuserdata'
+import SingleFakeData from './components/SingleFakeData/SingleFakeData';
 
 function App() {
+  const [fakeData,setFakeData] = useState([]);
+  console.log(fakeData);
 
-  const [allData, setAllData] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data => setAllData(data))
+    fetch('fakeData.json')
+    .then(res => res.json())
+    .then(data => setFakeData(data))
   }, [])
 
+
   return (
-    <div className='grid grid-cols-3'> 
-          {
-             allData.map(singleData => <Singleuserdata  key = {singleData.id} singleData={singleData}></Singleuserdata>)
-          }
+    <div className='flex items-center justify-center grid grid-cols-3'>
+      {/* <h1 className='text-3xl'>data length: {fakeData.length}</h1>  */}
+      {
+        fakeData.map(singleData => <SingleFakeData key = {singleData.id} singleData = {singleData}></SingleFakeData>)
+      }
     </div>
   )
 }
